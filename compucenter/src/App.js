ab4ee9423adbf39e65d7dd10f8a1f573b113a02c
sebasc99 <sebastiancardenassuarez@gmail.com>
2021-10-03 22:17:44 -0500

    Cambios en el Inicio

--- compucenter/src/App.js
+++ compucenter/src/App.js
@@ -19,41 +19,7 @@ function App() {
         </Switch>
       </Router>
 
-      <div class="banner">
-        <img src={banner} alt="" class="banner__img" />
-        <div class="contenedor">
-
-        </div>
-      </div>
-
-      <main class="main">
-        <div class="contenedor">
-          <section class="gestiones">
-            <h2 class="section__titulo">Modulos de Gestión</h2>
-            <div class="gestiones__columna" href="">
-              <img src={gestion_producto} alt="" class="gestion__imagen" />
-              <div class="gestion__descripcion">
-                <h3 class="gestion__titulo">Gestión de Productos</h3>
-                <p class="gestion__txt">Aqui va la descripción</p>
-              </div>
-            </div>
-            <div class="gestiones__columna">
-              <img src={gestion_ventas} alt="" class="gestion__imagen" />
-              <div class="gestion__descripcion">
-                <h3 class="gestion__titulo">Gestión de Ventas</h3>
-                <p class="gestion__txt">Aqui va la descripción</p>
-              </div>
-            </div>
-            <div class="gestiones__columna">
-              <img src={gestion_usuario} alt="" class="gestion__imagen" />
-              <div class="gestion__descripcion">
-                <h3 class="gestion__titulo">Gestión de Usuarios</h3>
-                <p class="gestion__txt">Aqui va la descripción</p>
-              </div>
-            </div>
-          </section>
-        </div>
-      </main>
+      <div className="Container" style={{ backgroundImage: 'url("../banner.jpg")', backgroundSize: 'cover' }}></div>
     </div >
   );
 }

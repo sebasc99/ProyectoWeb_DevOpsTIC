17fdd7b99dac8393febbf83e29f0b7d9bc5b1d80
sebasc99 <sebastiancardenassuarez@gmail.com>
2021-10-03 20:47:21 -0500

    Gestion de Productos

--- compucenter/src/App.js
+++ compucenter/src/App.js
@@ -9,11 +9,11 @@ function App() {
   return (
     <div className="App">
       <Router>
-          <Navbar />
-          <Switch>
-            <Route path="/productos" exact component={Productos} />
-          </Switch>
-        </Router>
+        <Navbar />
+        <Switch>
+          <Route path="/productos" exact component={Productos} />
+        </Switch>
+      </Router>
     </div>
   );
 }
